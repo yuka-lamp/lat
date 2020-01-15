edb1e6dc857308a2b4715543b9c31d5f95af670b
大西友香 <oonishiyuka@oonishiyukanoMacBook-ea.local>
2020-01-15 16:29:13 +0900

    ファイル追加

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+// CSSインポート
+import '../sass/app.scss';
+
+// JSインポート
+import 'bootstrap';
+// fortawesomeの設定
+FontAwesomeConfig = {
+  searchPseudoElements: true
+};
+import '../vendor/font-awesome/js/all.min.js';
--- submv-imgnone.php
+++ submv-imgnone.php
@@ -1,7 +1,7 @@
 <?php
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri(); ?>
-<section id="sub-mv">
+<section id="submv_imgnone" class="mv">
   <div class="wrap">
     <h2 class=""><?php echo get_the_title(); ?></h2>
   </div>
