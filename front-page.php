94def8c42a40167eabbb5fd1ae056589cb5f828f
大西友香 <oonishiyuka@oonishiyukanoMacBook-ea.local>
2020-01-15 18:44:54 +0900

    トップページ作成

--- front-page.php
+++ front-page.php
@@ -2,9 +2,96 @@
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
 get_header(); ?>
-<section class="sec">
-  <div class="wrap">
-
+<section id="about" class="sec">
+  <div class="wrap layout-01">
+    <h2 class="ttl2">お客様と、<br><span>暮らしのデザイン</span>を創る。</h2>
+    <div class="under flex">
+      <p>リードアーキテクトは設計と工事をワンストップで担当するため、お客様にとって最適な建築のご提案をフルオーダーで行うことができます。</p>
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/front/slide/photo01.png" alt="">
+      </div>
+    </div>
   </div>
+</section>
+<section id="service" class="sec">
+  <div class="ttl3">
+    <h2>service</h2>
+    <p>リードアーキテクトが手掛ける<br>手掛ける建設業</p>
+  </div>
+  <ul class="flex m-60">
+    <li>
+      <a href="<?php echo $home ?>">
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/front/service01.png" alt="">
+      </div>
+      <div class="txt-wrap">
+        <div>
+        <p class="main-color b">01</p>
+        <h3>注文建築</h3>
+        </div>
+        <div class="link">
+          <p class="main-color" href="<?php echo $home ?>/order">READ MORE ■</p>
+        </div>
+      </div>
+      </a>
+    </li>
+    <li>
+      <a href="<?php echo $home ?>">
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/front/service02.png" alt="">
+      </div>
+      <div class="txt-wrap">
+        <div class="">
+          <p class="main-color b">02</p>
+          <h3>リフォーム</h3>
+        </div>
+        <div class="link">
+          <p class="main-color" href="<?php echo $home ?>/order">READ MORE ■</p>
+        </div>
+      </div>
+      </a>
+    </li>
+    <li>
+      <a href="<?php echo $home ?>">
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/front/service03.png" alt="">
+      </div>
+      <div class="txt-wrap">
+        <div>
+        <p class="main-color b">03</p>
+        <h3>店舗建築</h3>
+        </div>
+        <div class="link">
+          <p class="main-color" href="<?php echo $home ?>/order">READ MORE ■</p>
+        </div>
+      </div>
+      </a>
+    </li>
+    <li>
+      <a href="<?php echo $home ?>">
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/front/service04.png" alt="">
+      </div>
+      <div class="txt-wrap">
+        <div>
+        <p class="main-color b">04</p>
+        <h3>大型建築</h3>
+        </div>
+        <div class="link">
+          <p class="main-color" href="<?php echo $home ?>/order">READ MORE ■</p>
+        </div>
+      </div>
+      </a>
+    </li>
+  </ul>
+</section>
+<section id="service" class="sec">
+  <div class="">
+    <div class="ttl3">
+      <h2>gallery</h2>
+    </div>
+    <p>リードアーキテクトが<br>今までお任せいただいた施工</p>
+  </div>
+  
 </section>
 <?php get_footer();
--- page-order.php
+++ page-order.php
@@ -7,7 +7,66 @@ get_header(); ?>
 
 <section class="sec">
   <div class="wrap">
+    <ul class="flex">
+      <li class="txt-c">
+        <p class="main-color b">01<br>helth</p>
+        <h2>健康住宅</h2>
+        <div class="img-wrap">
+          <img src="<?php echo $wp_url ?>/lib/images/front/order/01.png" alt="">
+        </div>
+      </li>
+      <li class="txt-c">
+        <p class="main-color b">02<br>design</p>
+        <h2>デザイン住宅</h2>
+        <div class="img-wrap">
+          <img src="<?php echo $wp_url ?>/lib/images/front/order/02.png" alt="">
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
 
+<section class="sec">
+  <div class="wrap">
+    <div class="ttl3">
+      <h2>gallery</h2>
+      <p>内覧ができるOB宅</p>
+    </div>
+    <?php
+    $arg = array(
+      'posts_per_page' => 3,
+      'orderby' => 'date',
+      'order' => 'DESC',
+      'category_name' => 'order',
+    );
+    $posts = get_posts($arg);
+    if ($posts): ?>
+    <ul class="">
+    <?php foreach ($posts as $post):
+    $t = get_the_title();
+    $i = get_the_post_thumbnail_url(get_the_ID(), 'medium');
+    $i_l = get_the_post_thumbnail_url(get_the_ID(), 'large');
+    $c = $post->post_content; // apply_filter( 'the_content', $s->post_content );
+    $url = get_permalink();
+    $cats = get_the_category(); ?>
+      <li>
+        <div class="img-wrap">
+          <a href="<?php echo $c; ?>" target="_blank">
+            <img src="<?php echo $i_l; ?>" srcset="<?php echo $i_l; ?> 1x,<?php echo $i_l; ?> 2x" alt="<?php echo $t; ?>">
+          </a>
+        </div>
+        <div class="txt-wrap">
+          <a href="<?php echo $url; ?>" target="_blank">
+            <p><?php echo $t; ?></p>
+            <div class="link">
+              <p class="main-color" href="<?php echo $home ?>/order">READ MORE ■</p>
+            </div>
+          </a>
+        </div>
+      </li>
+    <?php endforeach; ?>
+    </ul>
+    <?php endif; wp_reset_postdata(); ?>
   </div>
 </section>
 <?php get_footer();
