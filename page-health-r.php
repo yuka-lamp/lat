317055729fc30fc635ee081f601971fa08be2074
大西友香 <oonishiyuka@oonishiyukanoMacBook-ea.local>
2020-01-17 14:59:30 +0900

    特殊建築完成

---
+++ page-health-r.php
@@ -0,0 +1,154 @@
+<?php
+$home = esc_url(home_url());
+$wp_url = get_template_directory_uri();
+get_header(); ?>
+
+<?php get_template_part( 'submv-img' ); ?>
+
+<section id="" class="sec">
+  <div class="wrap layout-01">
+    <h2 class="ttl2">健康住宅で、<br><span>暮らし</span>を変えよう。</h2>
+    <div class="under flex">
+      <p>「国が認めた基準だから安心!」「大手メーカーだから間違いない!」
+        <br>家を建てる時も同様、誰しもがそんな常識を信じているものです。
+        <br>しかし、2003年以降シックハウスが社会問題化し、国が「建材から出るホルムアルデヒド、クロルピリホス量を制限するという法律制定」を行った後も、アレルギー症状をはじめとするシックハウス患者は増加傾向にあります。
+        <br>
+        <br>事実、建築業者のほとんどが、日本の住宅の短寿命の原因やシックハウスという事実を知りながらも国の定めた「最低限の基準」に則った家づくりをしております。</p>
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url ?>/lib/images/health-o/about.png" alt="健康住宅で暮らしを変えよう。">
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="about-0" class="sec">
+  <div class="wrap">
+    <div class="ttl3">
+      <h2>about</h2>
+      <p>リードアーキテクトの<br>ゼロ宣言の家とは？</p>
+    </div>
+    <h3 class="m-60">健康になるには運動と食事と「<span class="main-color">住環境</span>」。</h3>
+    <p class="gray">健康に密接に関係する3つに重点を置いた家づくりは0宣言の家とよばれ、本物の健康住宅として近畿、北陸、愛知県・三重県・和歌山県にて展開しております。健康住宅としてのエビデンス(科学的根拠)をすべて公開しておりますので、数ある自然素材住宅とは一線を画しており、安心安全なお家を提供いたしております。</p>
+    <ul class="flex ul-style-01 m-60">
+      <li>
+        <div class="txt-wrap">
+          <p class="main-color b">01</p>
+          <h2 class="m-30">断熱性能</h2>
+          <p class="gray txt-l">内部結露の発生を抑えることで、建物の耐久性を高め、住宅の長寿命化を実現しております。アトピーの原因とされるカビ・ダニを軽減し、健康的な住まいにします。</p>
+        </div>
+        <div class="img-wrap">
+          <img src="<?php echo $wp_url ?>/lib/images/health-o/about_01.png" alt="健康住宅">
+        </div>
+      </li>
+      <li>
+        <div class="txt-wrap">
+          <p class="main-color b">02</p>
+          <h2 class="m-30">調湿機能</h2>
+          <p class="gray txt-l">内部結露の発生を抑えることで、建物の耐久性を高め、住宅の長寿命化を実現しております。アトピーの原因とされるカビ・ダニを軽減し、健康的な住まいにします。</p>
+        </div>
+        <div class="img-wrap">
+          <img src="<?php echo $wp_url ?>/lib/images/health-o/about_02.png" alt="健康住宅">
+        </div>
+      </li>
+      <li>
+        <div class="txt-wrap">
+          <p class="main-color b">03</p>
+          <h2 class="m-30">断熱性能</h2>
+          <p class="gray txt-l">内部結露の発生を抑えることで、建物の耐久性を高め、住宅の長寿命化を実現しております。アトピーの原因とされるカビ・ダニを軽減し、健康的な住まいにします。</p>
+        </div>
+        <div class="img-wrap">
+          <img src="<?php echo $wp_url ?>/lib/images/health-o/about_03.png" alt="健康住宅">
+        </div>
+      </li>
+    </ul>
+    <div class="layout-02">
+      <h3>本当に健康を考えた素材</h3>
+      <ul class="slider-02 m-60">
+        <li class="flex">
+          <div class="img-wrap">
+            <img class="left" src="<?php echo $wp_url ?>/lib/images/health-o/slide/sozai_01.png" alt="無垢フローリング">
+          </div>
+          <div class="txt-wrap">
+            <p class="num gray">01</p>
+            <h4>無垢フローリング</h4>
+            <p class="gray">0宣言の家では健康・長持ち・快適という観点から、扱っているのは無垢フローリングのみ。木材独特のいい香りと、裸足で歩いた時の優しい肌触りが癒し効果を与えてくれます。</p>
+          </div>
+        </li>
+        <li class="flex">
+          <div class="img-wrap">
+            <img class="left" src="<?php echo $wp_url ?>/lib/images/health-o/slide/sozai_01.png" alt="無垢フローリング">
+          </div>
+          <div class="txt-wrap">
+            <p class="num gray">01</p>
+            <h4>無垢フローリング</h4>
+            <p class="gray">0宣言の家では健康・長持ち・快適という観点から、扱っているのは無垢フローリングのみ。木材独特のいい香りと、裸足で歩いた時の優しい肌触りが癒し効果を与えてくれます。</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
+<section id="doctor" class="sec">
+  <div class="wrap">
+    <div class="ttl3">
+      <h2>feature</h2>
+      <p>住医学研究会とすすめる<br>健康×家造り</p>
+    </div>
+    <h3 class="m-60">日々健康になる家造りを研究する「<span class="main-color">住医学研究会</span>」。</h3>
+    <p class="gray">「今の設計で問題はないのか」「この建材は本当に安全で長もちするのか」<br>日々の仕事を通じて、そんな不安や疑問を抱いた設計事務所や工務店が澤田升男氏が提唱する「0宣言の家」に共感して全国から集まり、医師や大学教授の協力のもと住まう人の健康を更に追求するため「住医学研究会」は誕生しました。</p>
+    <div class="layout-02 mt-60">
+      <h3>住医学研究会のご紹介</h3>
+      <ul class="flex">
+        <li>
+          <div class="img-wrap">
+            <img class="left" src="<?php echo $wp_url ?>/lib/images/health-o/doctor_01.png" alt="医師写真">
+          </div>
+          <h4>矢山 利彦<span>医師</span></h4>
+          <p class="gray">住医学研究会 理事長</p>
+        </li>
+        <li>
+          <div class="img-wrap">
+            <img class="left" src="<?php echo $wp_url ?>/lib/images/health-o/doctor_02.png" alt="医師写真">
+          </div>
+          <h4>矢山 利彦<span>医師</span></h4>
+          <p class="gray">住医学研究会 理事長</p>
+        </li>
+        <li>
+          <div class="img-wrap">
+            <img class="left" src="<?php echo $wp_url ?>/lib/images/health-o/doctor_03.png" alt="医師写真">
+          </div>
+          <h4>矢山 利彦<span>医師</span></h4>
+          <p class="gray">住医学研究会 理事長</p>
+        </li>
+      </ul>
+      <div class="btn mt-1 txt-c">
+        <a href="http://jyuigaku.com/" target="_blank">住医学研究会のHPへ</a>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="model">
+  <div class="sec mt-60">
+    <div class="wrap">
+      <div class="ttl3">
+        <h2>model house</h2>
+        <p>健康住宅<br>モデルハウス内覧会</p>
+      </div>
+      <div class="grid mt-60">
+        <div class="img-wrap">
+          <img class="left" src="<?php echo $wp_url ?>/lib/images/health-o/model.png" alt="モデルハウスの様子">
+        </div>
+        <div class="txt-wrap">
+          <h3>モデルハウスで<span class="main-color">本物</span>を実感。</h3>
+          <p>京都市左京区にあるゼロ宣言の家モデルハウスです。<br>リード・アーキテクトの家づくりに少しでも興味をもっていただいたお客様はどなたでもご宿泊可能です。<br>また、ご見学もご予約いただければいつでも可能ですので、お気軽にお電話で問い合せくださいませ。</p>
+          <div class="btn mt-2 txt-c">
+            <a href="http://jyuigaku.com/" target="_blank">TEL:0120-675-365</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_template_part( 'gallery' ); ?>
+<?php get_footer();
