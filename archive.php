307f0a7d9248c62e842065f45ac6e43f7e8cbfac
大西友香 <oonishiyuka@oonishiyukanoMacBook-ea.local>
2020-01-28 16:52:18 +0900

    一覧ページ修正

--- archive.php
+++ archive.php
@@ -3,19 +3,32 @@ $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
 get_header(); ?>
 
-<section id="submv_img" class="mv list">
+<section id="submv_list" class="mv list">
+  <div class="wrap">
     <h2 class="">事例紹介</h2>
+    <p class="gray ml-1">WORKS</p>
+  </div>
 </section>
 
-<section id="works" class="sec oblique">
-<div class="wrap cat">
-  <p class="b">カテゴリ一覧</p>
-  <ul>
-    <li><a href="<?php echo $home ?>/gallery">全て</a></li>
-    <?php wp_list_categories('title_li=&exclude=1'); ?>
-  </ul>
+<section id="works" class="sec oblique ">
+<div class="cat">
+  <div class="wrap">
+    <p class="b mb-1">カテゴリ一覧</p>
+    <ul>
+      <li><a href="<?php echo $home ?>/gallery">全て</a></li>
+      <?php wp_list_categories('title_li=&exclude=1'); ?>
+    </ul>
+  </div>
 </div>
 <div class="wrap">
+  <div class="list-ttl flex">
+    <h2 class="ttl3">WORKS<span>事例一覧</span></h2>
+    <?php
+      $custom_post_name = get_post_type_object(get_post_type())->name; // カスタム投稿タイプのスラッグを表示
+      $count_post = wp_count_posts( $custom_post_name , 'readable' ); // カスタム投稿タイプのスラッグを指定
+      echo '<p>'.$count_post->publish.'件</p>'; // 公開済の投稿数を表示
+    ?>
+  </div>
   <ul class="list">
     <?php if (have_posts()): while (have_posts()):
     the_post();
@@ -24,21 +37,21 @@ get_header(); ?>
         $t = mb_substr($t, 0, 28, 'UTF-8').'…';
     }
     $i = get_the_post_thumbnail_url(get_the_ID(), 'large');
-    $cats = get_the_category(); ?>
+    $cats = get_the_category();
+    ?>
       <li>
-        <a href="<?php the_permalink(); ?>" class="flex">
+        <a href="<?php the_permalink(); ?>" class="">
+        </p>
         <div class="img-wrap">
           <img class="" src="<?php echo $i; ?>" alt="<?php echo $t; ?>">
         </div>
         <div class="txt-wrap">
-          <p>
-            <?php foreach ($cats as $category) {
-                echo '<span>'.$category->name.'</span>';
-            } ?>
-            <span><?php echo get_the_date(); ?></span>
-          </p>
         <h3><?php echo $t; ?></h3>
-        <div class="content"><?php the_content(); ?></div>
+        <p>
+          <?php foreach ($cats as $category) {
+              echo '<span>by '.$category->name.'</span>';
+          } ?>
+        </p>
         </div>
       </a>
       </li>
