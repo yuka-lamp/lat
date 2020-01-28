2a65c4fb10be4557db6b750424a482c2cc2c2de3
大西友香 <oonishiyuka@oonishiyukanoMacBook-ea.local>
2020-01-28 17:40:48 +0900

    サブメインビジュアル修正

--- submv-imgnone.php
+++ submv-imgnone.php
@@ -1,8 +1,11 @@
 <?php
 $home = esc_url(home_url());
-$wp_url = get_template_directory_uri(); ?>
-<section id="submv_imgnone" class="mv">
+$wp_url = get_template_directory_uri();
+$slug = $post->post_name;
+?>
+<section id="submv_imgnone" class="mv list">
   <div class="wrap">
     <h2 class=""><?php echo get_the_title(); ?></h2>
+    <p class="gray ml-1"><?php echo attribute_escape($post->post_name); ?></p>
   </div>
 </section>
