120c04722f3529d72cb60940cf7dd10eba4edf51
大西友香 <oonishiyuka@oonishiyukanoMacBook-ea.local>
2020-01-29 16:33:11 +0900

    その他ページの充実

--- page-large.php
+++ page-large.php
@@ -24,6 +24,45 @@ get_header(); ?>
     </div>
   </div>
 </section>
+<section id="about-0" class="sec">
+  <div class="wrap">
+    <div class="ttl3">
+      <h2>point</h2>
+      <p>リードアーキテクトの<br>リノベーションの特徴</p>
+    </div>
+    <h3 class="m-60">設計と工事を<span class="main-color">ワンストップ</span>で</h3>
+    <p class="gray">設計と工事をワンストップで行うため、どんなデザインもできる。設計と工事をワンストップで行うため、どんなデザインもできる。設計と工事をワンストップで行うため、どんなデザインもできる。設計と工事をワンストップで行うため、どんなデザインもできる。</p>
+    <ul class="flex ul-style-02 mt-60">
+      <li class="txt-c">
+        <div class="img-wrap">
+          <img src="<?php echo $wp_url ?>/lib/images/design-o/about_01.png" alt="健康住宅">
+        </div>
+        <div class="txt-wrap">
+          <h3 class="">フルオーダーで<br>何でもできる</h3>
+          <p class="gray txt-l">この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</p>
+        </div>
+      </li>
+      <li class="txt-c">
+        <div class="img-wrap">
+          <img src="<?php echo $wp_url ?>/lib/images/design-o/about_02.png" alt="健康住宅">
+        </div>
+        <div class="txt-wrap">
+          <h3 class="">収納を考えた<br>トータル設計</h3>
+          <p class="gray txt-l">この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</p>
+        </div>
+      </li>
+      <li class="txt-c">
+        <div class="img-wrap">
+          <img src="<?php echo $wp_url ?>/lib/images/design-o/about_03.png" alt="健康住宅">
+        </div>
+        <div class="txt-wrap">
+          <h3 class="">鉄筋コンクリート<br>木造にも対応</h3>
+          <p class="gray txt-l">この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
 <?php
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri(); ?>
